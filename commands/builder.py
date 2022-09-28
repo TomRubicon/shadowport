e3395e333c1f47fb22f1093b882d26a7f7b4cdf2
John Milanowski <JohnMilanowski@gmail.com>
2022-09-27 18:20:56 -0700

    Add copytags switch to tunnel and dig commands.

--- commands/builder.py
+++ commands/builder.py
@@ -5,8 +5,10 @@ Custom builder commands
 import evennia
 from django.conf import settings
 from evennia import CmdSet
-from evennia.utils import create, utils, search, logger
+from evennia.utils import create, utils, search, logger, class_from_module
 from evennia.commands.default.building import ObjManipCommand
+
+COMMAND_DEFAULT_CLASS = class_from_module(settings.COMMAND_DEFAULT_CLASS)
 
 class CmdCoordDig(ObjManipCommand):
     """
@@ -19,6 +21,7 @@ class CmdCoordDig(ObjManipCommand):
 
     Switches:
        tel or teleport - move yourself to the new room
+       copytags - copy the current locations tags to the new room
 
     Examples:
        dig kitchen = north;n, south;s
@@ -33,7 +36,7 @@ class CmdCoordDig(ObjManipCommand):
     """
 
     key = "dig"
-    switch_options = ("teleport",)
+    switch_options = ("teleport", "copytags")
     locks = "cmd:perm(dig) or perm(Builder)"
     help_category = "Building"
 
@@ -192,9 +195,117 @@ class CmdCoordDig(ObjManipCommand):
                     alias_string,
                 )
         caller.msg("%s%s%s" % (room_string, exit_to_string, exit_back_string))
+        if "copytags" in self.switches:
+            for tag in location.tags.all():
+                new_room.tags.add(tag)
         if new_room and "teleport" in self.switches:
             caller.move_to(new_room)
+
+class CmdCoordTunnel(COMMAND_DEFAULT_CLASS):
+    """
+    create new rooms in cardinal directions only
+
+    Usage:
+      tunnel[/switch] <direction>[:typeclass] [= <roomname>[;alias;alias;...][:typeclass]]
+
+    Switches:
+      oneway - do not create an exit back to the current location
+      tel - teleport to the newly created room
+      copytags - copy curent locations tags to the new room
+
+    Example:
+      tunnel n
+      tunnel n = house;mike's place;green building
+
+    This is a simple way to build using pre-defined directions:
+     |wn,ne,e,se,s,sw,w,nw|n (north, northeast etc)
+     |wu,d|n (up and down)
+     |wi,o|n (in and out)
+    The full names (north, in, southwest, etc) will always be put as
+    main name for the exit, using the abbreviation as an alias (so an
+    exit will always be able to be used with both "north" as well as
+    "n" for example). Opposite directions will automatically be
+    created back from the new room unless the /oneway switch is given.
+    For more flexibility and power in creating rooms, use dig.
+    """
+
+    key = "tunnel"
+    aliases = ["tun"]
+    switch_options = ("oneway", "tel", "copytags")
+    locks = "cmd: perm(tunnel) or perm(Builder)"
+    help_category = "Building"
+
+    # store the direction, full name and its opposite
+    directions = {
+        "n": ("north", "s"),
+        "ne": ("northeast", "sw"),
+        "e": ("east", "w"),
+        "se": ("southeast", "nw"),
+        "s": ("south", "n"),
+        "sw": ("southwest", "ne"),
+        "w": ("west", "e"),
+        "nw": ("northwest", "se"),
+        "u": ("up", "d"),
+        "d": ("down", "u"),
+        "i": ("in", "o"),
+        "o": ("out", "i"),
+    }
+
+    def func(self):
+        """Implements the tunnel command"""
+
+        if not self.args or not self.lhs:
+            string = (
+                "Usage: tunnel[/switch] <direction>[:typeclass] [= <roomname>"
+                "[;alias;alias;...][:typeclass]]"
+            )
+            self.caller.msg(string)
+            return
+
+        # If we get a typeclass, we need to get just the exitname
+        exitshort = self.lhs.split(":")[0]
+
+        if exitshort not in self.directions:
+            string = "tunnel can only understand the following directions: %s." % ",".join(
+                sorted(self.directions.keys())
+            )
+            string += "\n(use dig for more freedom)"
+            self.caller.msg(string)
+            return
+
+        # retrieve all input and parse it
+        exitname, backshort = self.directions[exitshort]
+        backname = self.directions[backshort][0]
+
+        # if we received a typeclass for the exit, add it to the alias(short name)
+        if ":" in self.lhs:
+            # limit to only the first : character
+            exit_typeclass = ":" + self.lhs.split(":", 1)[-1]
+            # exitshort and backshort are the last part of the exit strings,
+            # so we add our typeclass argument after
+            exitshort += exit_typeclass
+            backshort += exit_typeclass
+
+        roomname = "Some place"
+        if self.rhs:
+            roomname = self.rhs  # this may include aliases; that's fine.
+
+        telswitch = ""
+        if "tel" in self.switches:
+            telswitch = "/teleport"
+        tagcopyswitch = ""
+        if "copytags" in self.switches:
+            tagcopyswitch = "/copytags"
+        backstring = ""
+        if "oneway" not in self.switches:
+            backstring = ", %s;%s" % (backname, backshort)
+
+        # build the string we will use to call dig
+        digstring = "dig%s%s %s = %s;%s%s" % (telswitch, tagcopyswitch, roomname, exitname, exitshort, backstring)
+        self.execute_cmd(digstring)
+
 
 class CustomBuilderCmdSet(CmdSet):
     def at_cmdset_creation(self):
         self.add(CmdCoordDig)
+        self.add(CmdCoordTunnel)
