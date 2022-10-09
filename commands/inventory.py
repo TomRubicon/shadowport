9845f04aef1bb61f1c2f84f169382ac50e2b1e2c
John Milanowski <JohnMilanowski@gmail.com>
2022-10-08 22:13:08 -0700

    Add new drop and get commands that allow for getting/dropping all items specified.

--- commands/inventory.py
+++ commands/inventory.py
@@ -6,12 +6,12 @@ Commands for manipulating the players inventory.
 """
 
 import re
-from collections import defaultdict
 import itertools
-import operator
 import evennia
 from evennia.commands.command import Command
+from evennia.commands.default.muxcommand import MuxCommand
 from evennia import CmdSet, utils
+
 
 class CmdInventory(Command):
     """
@@ -62,7 +62,7 @@ class CmdInventory(Command):
             string = f"|wYou are carrying: |n\n\n{table}\n\n|YTotal Weight:|n |M{total_weight}|n\n"
         self.caller.msg(string)
 
-class CmdPut(Command):
+class CmdPut(MuxCommand):
     """
     put
 
@@ -73,9 +73,15 @@ class CmdPut(Command):
     Put an item inside a container.
     """
 
-    pass
+    key = "put"
+    aliases = ["p", "place"]
+    help_category = "Inventory and Equipment"
+    locks = "cmd:all()"
+    
+    def func(self):
+        caller = self.caller
 
-class CmdGet(Command):
+class CmdGet(MuxCommand):
     """
     pick up something
 
@@ -90,48 +96,132 @@ class CmdGet(Command):
 
     key = "get"
     aliases = ["grab","pickup"]
+    help_category = "Inventory and Equipment"
     locks = "cmd:all()"
-    arg_regex = r"\s|$"
+    # switch_options = ("all")
 
     def func(self):
         """implements the command."""
 
         caller = self.caller
-
-        if not self.args:
+        location = caller.location
+        
+        if not self.lhs and not self.switches:
             caller.msg("Get what?")
             return
-        obj = caller.search(self.args.lstrip(), location=caller.location)
-        if not obj:
-            return
-        if caller == obj:
-            caller.msg("You can't get yourself.")
-            return
-        if not obj.access(caller, "get"):
-            if obj.db.get_err_msg:
-                caller.msg(obj.db.get_err_msg)
-            else:
-                caller.msg("You can't get that.")
-            return
-
-        # calling at_before_get hook method
-        if not obj.at_before_get(caller):
-            return
-
-        success = obj.move_to(caller, quiet=True)
-
-        if not success:
-            caller.msg("This can't be picked up.")
-        else:
-            caller.msg("You pick up %s." % obj.name)
-            caller.location.msg_contents(
-                    "%s picks up %s." % (caller.name, obj.name), exclude=caller
+        # get/all
+        elif not self.lhs and "all" in self.switches:
+            obj_list = [obj for obj in location.contents if obj != caller and obj.access(caller, "get")]
+        # get/all <obj>
+        elif self.lhs:
+            obj_list = caller.search(
+                self.lhs,
+                location=location,
+                use_nicks=True,
+                quiet=True
             )
-            # calling at_get hook method
-            obj.at_get(caller)
+
+        caller.msg(obj_list)
+        if not obj_list:
+            if self.lhs:
+                caller.msg(f"There is no {self.lhs} to get here.")
+            else:
+                caller.msg("There is nothing to get here.")
+            return
+
+        for obj in obj_list:
+            if not obj:
+                return
+            if caller == obj:
+                caller.msg("You can't get yourself.")
+                return
+            if not obj.access(caller, "get"):
+                if obj.db.get_err_msg:
+                    caller.msg(obj.db.get_err_msg)
+                else:
+                    caller.msg("You can't get that.")
+                return
+
+            # calling at_before_get mothod
+            if not obj.at_before_get(caller):
+                return
+
+            success = obj.move_to(caller, quiet=True)
+
+            if not success:
+                caller.msg("This can't be picked up.")
+            else:
+                caller.msg(f"You pick up {obj.name}.")
+                location.msg_contents(f"{caller.name} picks up {obj.name}.", exclude=caller)
+                obj.at_get(caller)
+            
+            if "all" not in self.switches:
+                return
+
+class CmdDrop(MuxCommand):
+    """
+    drop something
+
+    Usage:
+      drop <obj>
+      drop/all <obj>
+
+    Lets you drop an object from your inventory into the
+    location you are currently in.
+    """
+
+    key = "drop"
+    help_category = "Inventory and Equipment"
+    locks = "cmd:all()"
+
+    def func(self):
+        """implement command"""
+
+        caller = self.caller
+        
+        # drop
+        if not self.lhs and not self.switches:
+            caller.msg("Drop what?")
+            return
+        # drop/all 
+        elif not self.lhs and "all" in self.switches:
+            obj_list = [obj for obj in caller.contents]
+        # drop/all <obj>
+        elif self.lhs: 
+            obj_list = caller.search(
+                self.lhs,
+                location=caller,
+                use_nicks=True,
+                quiet=True
+            ) 
+
+        if not obj_list:
+            caller.msg(f"You aren't carrying {self.lhs}.")
+            return
+
+        for obj in obj_list:
+            if not obj:
+                return
+
+            # Call the object script's at_before_drop() method.
+            if not obj.at_before_drop(caller):
+                return
+
+            success = obj.move_to(caller.location, quiet=True)
+            if not success:
+                caller.msg(f"{obj.name} couldn't be dropped.")
+            else:
+                caller.msg(f"You drop {obj.name}.")
+                caller.location.msg_contents(f"{caller.name} drops {obj.name}", exclude=caller)
+                obj.at_drop(caller)
+
+            # Only drop the first item if all is not specified.
+            if "all" not in self.switches:
+                return
 
 class InventoryCmdSet(CmdSet):
     def at_cmdset_creation(self):
         self.add(CmdInventory)
-        # self.add(CmdGet)
+        self.add(CmdGet)
         self.add(CmdPut)
+        self.add(CmdDrop)
