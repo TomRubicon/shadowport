373ed4d0de300c18afc281f96219e81a1963e7dc
John Milanowski <JohnMilanowski@gmail.com>
2022-10-31 19:34:12 -0700

    Fix #33. Emoting in a dark room properly hides names.

--- commands/social.py
+++ commands/social.py
@@ -49,6 +49,57 @@ class CmdYell(BaseCommand):
                 dir = get_direction(coords, room_coords)
                 room.msg_contents(f'From the {dir} you hear someone yell, "{msg}".', exclude=self.caller)
 
+class CmdPose(BaseCommand):
+    """
+    strike a pose
+
+    Usage:
+      pose <pose text>
+      pose's <pose text>
+
+    Example:
+      pose is standing by the wall, smiling.
+       -> others will see:
+      Tom is standing by the wall, smiling.
+
+    Describe an action being taken. The pose text will
+    automatically begin with your name.
+    """
+
+    key = "pose"
+    aliases = [":", "emote"]
+    locks = "cmd:all()"
+
+    def parse(self):
+        """
+        Custom parse the cases where the emote
+        starts with some special letter, such
+        as 's, at which we don't want to separate
+        the caller's name and the emote with a
+        space.
+        """
+        args = self.args
+        if args and not args[0] in ["'", ",", ":"]:
+            args = " %s" % args.strip()
+        self.args = args
+
+    def func(self):
+        """Hook function"""
+        if not self.args:
+            msg = "What do you want to do?"
+            self.caller.msg(msg)
+        else:
+            msg = self.args
+            rm.dark_aware_msg(
+                '{caller}{msg}',
+                self.caller.location,
+                {"{caller}":self.caller.name, "{msg}":msg},
+                {"{caller}":"Someone", "{msg}":msg}
+            )
+
+
+
 class SocialCmdSet(CmdSet):
     def at_cmdset_creation(self):
         self.add(CmdYell)
+        self.add(CmdPose)
